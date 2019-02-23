5e16187d6a8a6fccf7ed5ec4dfd69635c29d6ad2
Joey Baldwin <baldwinjj@g.cofc.edu>
2019-02-23 16:54:29 -0500

    login

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginService } from './services/login.service';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ], 
+  providers: [
+    LoginService
   ]
 })
 export class LoginModule { }
